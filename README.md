e209dce1ccb9d37ffa9c515772802fa68a4781f0
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2022-04-10 09:38:16 +0900

    受付画面がパースできない版

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tokyo Metro Library Tama Reservation
+
+## References
+
+https://github.com/mattsse/voyager
+https://github.com/mikaelmello/inquire
+https://docs.rs/confy/0.3.1/confy/
+
+https://qiita.com/YoshiTheQiita/items/f66828d61293c75a4585
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use voyager::{Collector, Crawler, CrawlerConfig, RequestDelay, Response, Scraper
 use anyhow::Result;
 use std::time::Duration;
 use futures::StreamExt;
-use reqwest::Url;
+use inquire::{error::InquireError, Select};
 
 struct TamaLibCrawler {
-    date_scraper: Selector
+    date_scraper: Selector,
+    datetime_scraper: Selector,
 }
 
 impl Default for TamaLibCrawler {
     fn default() -> Self {
         Self {
-            date_scraper: Selector::parse("table:nth-of-type(2) a").unwrap()
+            date_scraper: Selector::parse("table:nth-of-type(2) a").unwrap(),
+            datetime_scraper: Selector::parse("table tr:nth-child(5) input[type=radio]").unwrap()
         }
     }
 }
@@ -20,12 +22,14 @@ impl Default for TamaLibCrawler {
 #[derive(Debug)]
 enum TamaLibState {
     Index,
-    Uketsuke,
+    Uketsuke(Date),
 }
 
 #[derive(Debug)]
 struct Date {
     date: String,
+    url: String,
+    times: Vec<String>
 }
 
 impl Scraper for TamaLibCrawler {
@@ -38,15 +42,28 @@ impl Scraper for TamaLibCrawler {
         if let Some(state) = response.state {
             match state {
                 TamaLibState::Index => {
-                    for date in html
+                    for (date, url) in html
                         .select(&self.date_scraper)
-                        .map(|el| el.text().collect::<Vec<_>>().join(""))
+                        .map(|el| (el.text().collect::<Vec<_>>().join(""), el.value().attr("href").map(str::to_string).unwrap()))
                         {
-                            println!("{}", date);
+                            crawler.visit_with_state(
+                                url.clone(),
+                                TamaLibState::Uketsuke(Date{
+                                    date: date,
+                                    url: url,
+                                    times: Vec::new()
+                                }),
+                            );
                         }
                 }
-                TamaLibState::Uketsuke => {
-                    println!("Uketsuke");
+                TamaLibState::Uketsuke(mut date) => {
+                    for datetime in html
+                        .select(&self.datetime_scraper)
+                        .filter_map(|el| el.value().attr("value").map(str::to_string))
+                        {
+                            date.times.push(datetime);
+                        }
+                    return Ok(Some(date));
                 }
             }
         }
@@ -57,9 +74,11 @@ impl Scraper for TamaLibCrawler {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let config = CrawlerConfig::default().allow_domain_with_delay(
-        "www.library.metro.tokyo.lg.jp",
-         RequestDelay::Fixed(Duration::from_millis(2_000))
+    let config = CrawlerConfig::default().respect_robots_txt().allow_domains_with_delay(
+        [
+            ("www.library.metro.tokyo.lg.jp", RequestDelay::Fixed(Duration::from_millis(2_000))),
+            ("www.shinsei.elg-front.jp", RequestDelay::Fixed(Duration::from_millis(1_000)))
+            ],
     );
     let mut collector = Collector::new(TamaLibCrawler::default(), config);
 
@@ -68,12 +87,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         TamaLibState::Index
     );
 
-
     while let Some(output) = collector.next().await {
-        if let Ok(post) = output {
-            println!("Post: {:?}", post)
+        if let Ok(date) = output {
+            println!("Date: {:?}", date)
         }
     }
+
+    // let options: Vec<&str> = vec!["Banana", "Apple", "Strawberry", "Grapes", "Lemon", "Tangerine", "Watermelon", "Orange", "Pear", "Avocado", "Pineapple"];
+
+    // let ans: Result<&str, InquireError> = Select::new("Date", options).prompt();
+
+    // match ans {
+    //     Ok(choice) => println!("{}! That's mine too!", choice),
+    //     Err(_) => println!("There was an error, please try again"),
+    // }
 
     Ok(())
 }
