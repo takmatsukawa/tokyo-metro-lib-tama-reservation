36eeeeef98b1ddcc701846cf014b6881c511737f
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2022-04-10 11:45:04 +0900

    curlでも変わらず

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,25 @@ use std::time::Duration;
 use futures::StreamExt;
 use inquire::{error::InquireError, Select};
 use reqwest;
+use std::io::{stdout, Write};
 
+use curl::easy::Easy;
+use encoding_rs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    // セレクターをパース　(このセレクターは記事のアンカーノード群(タイトル)を指す。 <a href="link">Title</a>)
-    let selector = scraper::Selector::parse("table tr:nth-child(5) input[type=radio]").unwrap();
+    let mut easy = Easy::new();
+    easy.url("https://www.shinsei.elg-front.jp/tokyo2/uketsuke/form.do?id=1648881164572").unwrap();
+    easy.write_function(|data| {
+        let (res, _, _) = encoding_rs::SHIFT_JIS.decode(data);
 
-    // `https://blog.rust-lang.org/` へHTTPリクエスト
-    let client = reqwest::Client::builder()
-        .redirect(reqwest::redirect::Policy::none())
-        .build()?;
-    let body: String = client.get("https://www.shinsei.elg-front.jp/tokyo2/navi/index.html").send().await?.text().await?;
+        stdout().write_all(res.as_bytes()).unwrap();
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap();
 
-    println!("{body}");
-    // HTMLをパース
-    let document = scraper::Html::parse_document(&body);
-
-    // セレクターを用いて要素を取得
-    let elements = document.select(&selector);
-
-    // 全記事名を出力
-    elements.for_each(|e| println!("{}", e.text().next().unwrap()));
-
-    // 一件目の記事名
-    // assert_eq!(elements.next().unwrap().text().next().unwrap(), "Announcing Rust 1.50.0");
-    // 二件目の記事名
-    // assert_eq!(elements.next().unwrap().text().next().unwrap(), "mdBook security advisory");
-    // 三件目の記事名
-    // assert_eq!(elements.next().unwrap().text().next().unwrap(), "Announcing Rust 1.49.0");
-    // 最古の記事名
-    // assert_eq!(elements.last().unwrap().text().next().unwrap(), "Road to Rust 1.0");
+    println!("{}", easy.response_code().unwrap());
 
 
     Ok(())
